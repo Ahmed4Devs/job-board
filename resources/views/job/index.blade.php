9ded7486230a99d47ff24c396cd8fabeb8b6eee9
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-06-10 11:41:37 +0300

    Start using Blade template and embed Tailwind css in our application

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,8 +2,6 @@
   <h1>Job Board</h1>
   <h3>My name is {{ $name }}</h3>
   @foreach ($jobs as $job)
-    <div>
-    {{ $job['title'] }}: {{ $job['salary'] }}
-    </div>
+    <div>{{ $job['title'] }}: {{ $job['salary'] }}</div>
   @endforeach
 </div>
