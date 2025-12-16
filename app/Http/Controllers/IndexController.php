584f3a7e5bfd227f5784a41dcc1112cb871cf9a9
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-16 19:03:13 +0300

    Refactor code structure for improved readability and maintainability

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,19 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
+
 {
-    function index()
-    {
-        return view("index");
-    }
+  function index()
+  {
+    return view("index");
+  }
 
-    function about()
-    {
-        return view("about", ["pageTitle" => "About Page"]);
-    }
+  function about()
+  {
+    return view("about", ["pageTitle" => "About Page"]);
+  }
 
-    function contact()
-    {
-        return view("contact", ["pageTitle" => "Contact Page"]);
-    }
+  function contact()
+  {
+    return view("contact", ["pageTitle" => "Contact Page"]);
+  }
 }
