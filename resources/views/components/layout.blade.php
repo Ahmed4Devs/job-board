55f0dd1e31db33050a83b967dbaa72fa1702bdd2
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-17 13:19:33 +0300

    Exploring blade template engine

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Job Board | {{ $title ?? 'Onboarding top talent' }}</title>
+</head>
+
+<body>
+  <h1>Job Board</h1>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+  </nav>
+
+  {{ $slot }}
+
+</body>
+
+</html>
