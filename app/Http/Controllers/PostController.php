0c5661fc622a05ba6e19da03502d6917bb56960f
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-23 09:19:12 +0300

    Explore eloquent funtions: create, find, first

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $data = Post::all();
+
+        return view('post.index', ['posts' => $data, "pageTitle" => "Blog"]);
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        // handle null -> message
+
+        return view('post.show', ['post' => $post, "pageTitle" => $post->title]);
+    }
+
+    public function create()
+    {
+        $post = Post::create([
+            'title' => 'My find unique post',
+            'body' => 'This is to test find',
+            'author' => 'Ahmed',
+            'published' => true
+        ]);
+
+        return redirect('/blog');
+    }
+}
