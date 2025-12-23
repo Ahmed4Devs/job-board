521f2d5671ac9f2b257c8bc3f53d62a63b6a8c14
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-23 08:08:21 +0300

    Implements Migrations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    protected $fillable = ['title', 'body', 'published']; // fields can be updated
+    protected $fillable = ['title', 'body', 'author', 'published']; // fields can be updated
 
     protected $guarded = ['id']; // cannot be updated/assigned (readonly)
 }
