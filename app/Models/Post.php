96b8fb624b66c9ddf2ba5baac5976a7a35d6f162
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-26 20:55:35 +0300

    Add Tag model and pivot table for many-to-many relationship with Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use \Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 
 class Post extends Model
 {
@@ -16,5 +18,10 @@ class Post extends Model
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    //
+    protected $table = 'tag';
+
+    protected $fillable = ['title']; // fields can be updated
+
+    protected $guarded = ['id']; // cannot be updated/assigned (readonly)
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
