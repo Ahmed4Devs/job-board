42294ca3f59c41802e655f9561049d7abca1b558
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-27 12:13:06 +0300

    Implement Pagination, simple pagination and cursor pagination

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,7 +1,9 @@
 <x-layout :title="$pageTitle">
   <h2>Blog Page</h2>
   @foreach ($posts as $post)
-    <h1 class="text-2xl">{{ $post->title }}</h1>
+    <h1 class="text-2xl">{{ $post->id }}: {{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
   @endforeach
+
+  {{ $posts->links() }}
 </x-layout>
