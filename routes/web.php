12650abdc85d3537b949fd4468f18a23eef4bbd5
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-28 15:55:49 +0300

    Refactor controllers and routes for improved structure and clarity; implement PostApiController for RESTful API handling.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\PostController;
@@ -7,9 +9,10 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index']);
-Route::get('/about', [IndexController::class, 'about']);
-Route::get('/contact', [IndexController::class, 'contact']);
+Route::get('/', IndexController::class);
+Route::get('/about', AboutController::class);
+Route::get('/contact', ContactController::class);
+
 Route::get('/job', [JobController::class, 'index']);
 
 Route::resource('blog', PostController::class);
